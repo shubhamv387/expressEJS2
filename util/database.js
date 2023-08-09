2e16fe89a8777f9468b1a7a5c9ee78f26a3db4ca
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-09 15:17:33 +0530

    created new product model using sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "Shubham@@387", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Shubham@@387",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
